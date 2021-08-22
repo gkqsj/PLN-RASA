abbc79bb36c3507473646b78e53de9eb025ae0ef
Gabriel <gkqsj@cin.ufpe.br>
2021-08-22 04:48:01 -0300

    Lógica consertada, casos de teste adicionados

--- RASA/actions/actions.py
+++ RASA/actions/actions.py
@@ -1,61 +1,80 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
-#
+
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-# from typing import Any, Text, Dict, List
+from typing import Any, Text, Dict, List
+import json
+import random
+import datetime as dt
 
-# from rasa.sdk.events import SlotSet
-# from rasa.sdk import Action , Tracker
-# from rasa.sdk.executor import CollectingDispatcher
+from rasa_sdk.events import AllSlotsReset
+from rasa_sdk import Action , Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# def findTrain(ent):
-#     return f"found"
 
-# class ActionLookForTrain(Action):
+with open ('db.json', 'r') as read_file:
+    trains_in_db = json.load(read_file)
 
-#     def name(self) -> Text:
-#         return "action_look_for_train"
+entities = {}
 
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+def select_trains(train):
+    train_leave_at = dt.datetime.strptime(train["leaveAt"], '%H:%M')
+    train_arrive_by = dt.datetime.strptime(train["arriveBy"], '%H:%M')
 
-#         ent = tracker.latest_message['entities'][0]
-#         print(ent)
+    entity_leave_at = dt.datetime.strptime(entities['leaveat'], '%H:%M') 
+    entity_arrive_by = dt.datetime.strptime(entities['arriveby'], '%H:%M')
 
-#         dispatcher.utter_message(text=findTrain(ent))
+    return train["departure"] == entities['departure'] and train["destination"] == entities['destination'] and train["day"] == entities['day'] and train_leave_at >= entity_leave_at and train_arrive_by <= entity_arrive_by
 
-#         return [SlotSet("train-destination",ent["train-destination"]),
-#                 SlotSet("train-departure",ent["train-departure"]),
-#                 SlotSet("train-day",ent["train-day"]),
-#                 SlotSet("train-bookpeople",ent["train-bookpeople"]),
-#                 SlotSet("train-arriveby",ent["train-arriveby"]),
-#                 SlotSet("train-leaveat",ent["train-leaveat"]),
-#         ]
+def get_trains(): 
+    options = [x for x in filter(select_trains, trains_in_db)] 
 
-# class ActionBookTrain(Action):
+    random.shuffle(options) 
+    options = options[:min(5, len(options))] 
 
-#     def name(self) -> Text:
-#         return "action_book_train"
+    # print(options)
 
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    return options
 
-#         ent = tracker.latest_message['entities'][0]
-#         print(ent)
+class ActionFoundYourTrainWithHour(Action):
+
+    def name(self) -> Text:
+        return "action_found_your_train_with_hour"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        global entities
+        entities = {}
+        entities['departure'] = tracker.get_slot('train-departure').lower()
+        entities['destination'] = tracker.get_slot('train-destination').lower()
+        entities['day'] = tracker.get_slot('train-day').lower()
+        entities['arriveby'] = tracker.get_slot('train-arriveby')
+        entities['leaveat'] = tracker.get_slot('train-leaveat')
         
-#         dispatcher.utter_message(text=findTrain(ent))
 
-#         return [SlotSet("train-destination",ent["train-destination"]),
-#                 SlotSet("train-departure",ent["train-departure"]),
-#                 SlotSet("train-day",ent["train-day"]),
-#                 SlotSet("train-bookpeople",ent["train-bookpeople"]),
-#                 SlotSet("train-arriveby",ent["train-arriveby"]),
-#                 SlotSet("train-leaveat",ent["train-leaveat"]),
-#         ]
+        train_results = get_trains()
+
+        buttons = [{"title":'train code: ' + x["trainID"] + ' - ' + 'Arrive by: ' + x["arriveBy"] + ', Leave at: ' + x['leaveAt'], "payload": '/book_train{"trainID":"' + x["trainID"] + '"}'} for x in train_results] + [{"title": "Finished", "payload": "/finish_query"}]
+        dispatcher.utter_message(text="here's what I found:", buttons=buttons)
+
+        return []
+
+class ActionResetAllSlots(Action):
+
+    def name(self) -> Text:
+        return "action_reset_all_slots"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        return [AllSlotsReset()]
+
+
 
 # class ActionGetTrivia(Action):
 
@@ -84,26 +103,3 @@
 #                 SlotSet("train-arriveby",ent["train-arriveby"]),
 #                 SlotSet("train-leaveat",ent["train-leaveat"]),
 #         ]
-
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
